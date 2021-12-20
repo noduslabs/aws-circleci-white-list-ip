041715009546699e5c42cb64058726a5a64ac38f
Dmitry <info@noduslabs.com>
2021-12-20 15:10:37 +0100

    changes

--- src/scripts/add.sh
+++ src/scripts/add.sh
@@ -1,4 +1,4 @@
-source '$(pwd)'/src/scripts/common.sh
+source '$(pwd)/src/scripts/common.sh'
 
 Add() {
   Common
@@ -10,7 +10,7 @@ Add() {
   echo "Allowing CircleCI to access port ${PARAM_PORT} from IP ${IP} to the security group ${PARAM_GROUPID}"
 
   ${AWS_COMMAND} ec2 authorize-security-group-ingress --group-id "${PARAM_GROUPID}" --ip-permissions \
-    '$(echo '[{"IpProtocol": "tcp", "FromPort": 443, "ToPort": 443, "IpRanges": [{"CidrIp": "", "Description": "CircleCI"}]}]' | jq -c '.[].IpRanges[].CidrIp="'"${IP}"/"${PARAM_MASK}"'"|.[].IpRanges[].Description="'"${PARAM_DESC}"'"|.[].FromPort='"${PARAM_PORT}"'|.[].ToPort='"${PARAM_PORT}"'')'
+    '$(echo '[{"IpProtocol": "tcp", "FromPort": 443, "ToPort": 443, "IpRanges": [{"CidrIp": "", "Description": "CircleCI"}]}]' | jq -c '.[].IpRanges[].CidrIp="'""${IP}""/""${PARAM_MASK}""'"|.[].IpRanges[].Description="'""${PARAM_DESC}""'"|.[].FromPort='"${PARAM_PORT}"'|.[].ToPort='"${PARAM_PORT}"'')'
 }
 
 ORB_TEST_ENV="bats-core"
